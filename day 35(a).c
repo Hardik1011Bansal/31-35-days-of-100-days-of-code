9fdcd3b695177444ab3993a31fc0f7b560e09033
Hardik1011Bansal <bansalsahab1011@gmail.com>
2025-09-24 16:58:16 +0530

    Create day 35(a).c

---
+++ day 35(a).c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    int n;
+    printf("Enter number of elements in the array :");
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter the %d entry of the array :",i);
+        scanf("%d",&a[i]);
+    }
+    int largest=a[0];
+    int sec_largest=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>largest)
+        {
+            largest=a[i];
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        if((a[i]!=largest) &&(a[i]>sec_largest))
+        {
+            sec_largest=a[i];
+        }
+    }
+    
+    if(sec_largest==INT_MIN)
+    {
+        printf("All the elements are equal");
+    }
+    else
+    {
+          printf("%d",sec_largest);
+    }
+    
+    
+  
+    
+    return 0;
+}
