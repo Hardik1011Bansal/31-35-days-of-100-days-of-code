22abc9b68eaf6fc42daf265484cbdab8104b9271
Hardik1011Bansal <bansalsahab1011@gmail.com>
2025-10-21 02:26:00 +0530

    Update day 35(a).c

--- day 35(a).c
+++ day 35(a).c
@@ -1,7 +1,7 @@
 //Q69 (Arrays (1D))
 //Find the second largest element in an array
 #include<stdio.h>
-#include<limits.h>
+
 int main()
 {
     int n;
@@ -14,7 +14,7 @@ int main()
         scanf("%d",&a[i]);
     }
     int largest=a[0];
-    int sec_largest=INT_MIN;
+    int sec_largest=a[0];
     for(int i=0;i<n;i++)
     {
         if(a[i]>largest)
@@ -29,15 +29,8 @@ int main()
             sec_largest=a[i];
         }
     }
-    
-    if(sec_largest==INT_MIN)
-    {
-        printf("All the elements are equal");
-    }
-    else
-    {
           printf("%d",sec_largest);
-    }
+    
     
     
   
